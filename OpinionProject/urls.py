02a282812cfa1b976d9a3c0caffcafc037fc5439
Twenty-One-Do <dnjseh8962@gmail.com>
2024-05-14 16:35:21 +0900

    Project Init
    - 프로젝트 기본 설정 완료
    - 각 app url연결
    - 장고 버전 requirements에 기술되어 있으니 처음 clone 후 pip install -r requirements.txt 요망

---
+++ OpinionProject/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/onions/', include('onions.urls')),
+    path('api/pies/', include('pies.urls')),
+    path('api/profiles/', include('profiles.urls')),
+    path('api/votes/', include('votes.urls')),
+]
