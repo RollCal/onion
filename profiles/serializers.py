85f9e23c07b65f913b173699c97fadd8acc807a5
ryuh2929 <ryuh2929@gmail.com>
2024-05-17 17:42:16 +0900

    UPDATE:profile조회에 작성 pie 목록 추가

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from pies.models import Pie
 
+class ProfilePieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pie
+        fields = [
+            'id',
+            'object_id',
+            'color',
+            'title',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = [
+            'id',
+            'object_id',
+            'color',
+            'title',
+            'created_at',
+            'updated_at',
+        ]
 class ProfileSerializer(serializers.ModelSerializer):
+    pie_set = ProfilePieSerializer(many=True, read_only=True)
     class Meta:
         model = get_user_model()
         fields = ['id',
@@ -13,6 +34,7 @@ class ProfileSerializer(serializers.ModelSerializer):
                   'birth',
                   'karma',
                   'date_joined',
+                  'pie_set',
                   ]
         read_only_fields = ['id',
                             'username',
@@ -22,7 +44,9 @@ class ProfileSerializer(serializers.ModelSerializer):
                             'birth',
                             'karma',
                             'date_joined',
+                            'pie_set',
                             ]
+    #비밀번호 제외
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         ret.pop('password')
