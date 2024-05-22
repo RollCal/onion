52acc2a79ec3ab52bf174b4505128e6e3a9bd472
Twenty-One-Do <dnjseh8962@gmail.com>
2024-05-22 20:04:35 +0900

    UPDATE: OVlist

--- onions/models.py
+++ onions/models.py
@@ -6,7 +6,7 @@ class Onion(models.Model):
     title = models.CharField(max_length=30)
     color = models.CharField(choices=(('Orange','Orange'), ('Purple','Purple')), max_length=10)
     parent_onion = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='child_onions')
-    num_of_views = models.IntegerField(default=0)
+    num_of_views = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- onions/serializers.py
+++ onions/serializers.py
@@ -1,8 +1,15 @@
+from collections import deque
+from itertools import chain
+
+from django.db.models import Count
 from rest_framework import serializers
 from onions.models import Onion, OnionVersus
 from django.shortcuts import get_object_or_404
 
 class OnionSerializer(serializers.ModelSerializer):
+
+    up_vote_num = serializers.SerializerMethodField()
+    down_vote_num = serializers.SerializerMethodField()
 
     class Meta:
         model = Onion
@@ -14,6 +21,12 @@ class OnionSerializer(serializers.ModelSerializer):
                             'created_at',
                             'updated_at')
 
+    def get_up_vote_num(self, obj):
+        return obj.votes.filter(type='Up').count()
+
+    def get_down_vote_num(self, obj):
+        return obj.votes.filter(type='Down').count()
+
 class OnionDetailSerializer(OnionSerializer):
 
     children = serializers.SerializerMethodField()
@@ -21,6 +34,16 @@ class OnionDetailSerializer(OnionSerializer):
     def get_children(self, obj):
         children = obj.child_onions.all()
         return OnionDetailSerializer(children, many=True).data
+
+class OnionVisualizeSerializer(OnionSerializer):
+
+    next = serializers.SerializerMethodField()
+
+    def get_next(self, obj):
+        children = obj.child_onions.annotate(children_votes_count=Count('votes')).order_by('-children_votes_count')
+        if children.exists():
+            next_child = children[0]
+            return OnionVisualizeSerializer(next_child).data
 
 
 class OnionVersusSerializer(serializers.ModelSerializer):
@@ -32,6 +55,7 @@ class OnionVersusSerializer(serializers.ModelSerializer):
 class OVListSerializer(OnionVersusSerializer):
     orange_onion = serializers.SerializerMethodField()
     purple_onion = serializers.SerializerMethodField()
+
     def get_orange_onion(self, obj):
         orange_ins = get_object_or_404(Onion, pk=obj.orange_onion_id)
         return OnionSerializer(orange_ins).data
