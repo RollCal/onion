aadda0f84ff4ea4428e8db62053ed79db0e7e15f
ryuh2929 <ryuh2929@gmail.com>
2024-06-03 09:10:55 +0900

    UPDATE:report 중복방지

--- management/models.py
+++ management/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
@@ -16,3 +17,13 @@ class Report(models.Model):
     report_content = models.TextField()
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    # 신고글 id, type, 유저가 모두 중복 되는지 체크
+    def save(self):
+        if Report.objects.filter(
+            object_id=self.object_id,
+            content_type=self.content_type,
+            writer=self.writer
+        ).exists():
+            raise ValidationError('이미 신고한 글 입니다')
+        super().save()
